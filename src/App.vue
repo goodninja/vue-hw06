9a01411ab528445ea518ae99cee2004bbbfd20a5
Ivan_Baryshev <obivanivan@mail.ru>
2023-09-12 17:24:18 +0300

    hw06 made our projects page with components

---
+++ src/App.vue
@@ -0,0 +1,166 @@
+<template>
+  <div id="app">
+    <HeaderComp />
+    <div class="button__container center">
+      <button
+        class="button__checked"
+        :class="{ 'tag-clicked': tag === filterItem }"
+        @click="clickTag(tag)"
+        v-for="tag in getTags"
+        :key="tag.id"
+      >
+        {{ tag }}
+      </button>
+    </div>
+    <CardComp :cards="filterCards" />
+    <FooterComp />
+  </div>
+</template>
+
+<script>
+import CardComp from "./components/CardComp.vue";
+import FooterComp from "./components/FooterComp.vue";
+import HeaderComp from "./components/HeaderComp.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      cards: [
+        {
+          title: "Minimal Bedroom",
+          text: "Decor / Artchitecture",
+          tag: "BedRoom",
+          img: "img/card_prod.png",
+        },
+        {
+          title: "Minimal Bedroom",
+          text: "Decor / Artchitecture",
+          tag: "BedRoom",
+          img: "img/card_prod.png",
+        },
+
+        {
+          title: "Classic Minimal Bedroom",
+          text: "Decor / Artchitecture",
+          tag: "BedRoom",
+          img: "img/card_prod.png",
+        },
+        {
+          title: "Modern Bedroom",
+          text: "Decor / Artchitecture",
+          tag: "BedRoom",
+          img: "img/card_prod.png",
+        },
+        {
+          title: "Classic Minimal Bathroom",
+          text: "Decor / Artchitecture",
+          tag: "Bathroom",
+          img: "img/card_prod2.png",
+        },
+        {
+          title: "Modern Bathroom",
+          text: "Decor / Artchitecture",
+          tag: "Bathroom",
+          img: "img/card_prod2.png",
+        },
+        {
+          title: "Minimal Bathroom",
+          text: "Decor / Artchitecture",
+          tag: "Bathroom",
+          img: "img/card_prod2.png",
+        },
+        {
+          title: "Minimal Bathroom",
+          text: "Decor / Artchitecture",
+          tag: "Bathroom",
+          img: "img/card_prod2.png",
+        },
+        {
+          title: "Minimal Kitchen",
+          text: "Decor / Artchitecture",
+          tag: "Kitchen",
+          img: "img/card_prod3.png",
+        },
+        {
+          title: "Minimal Kitchen",
+          text: "Decor / Artchitecture",
+          tag: "Kitchen",
+          img: "img/card_prod3.png",
+        },
+        {
+          title: "Classic Minimal Kitchen",
+          text: "Decor / Artchitecture",
+          tag: "Kitchen",
+          img: "img/card_prod4.png",
+        },
+        {
+          title: "Classic Minimal Kitchen",
+          text: "Decor / Artchitecture",
+          tag: "Kitchen",
+          img: "img/card_prod4.png",
+        },
+      ],
+      filterItem: "Kitchen",
+    };
+  },
+  components: {
+    CardComp,
+    HeaderComp,
+    FooterComp,
+  },
+  computed: {
+    getTags() {
+      let tags = [];
+      this.cards.forEach((el) => {
+        tags.push(el.tag);
+      });
+      return new Set(tags);
+    },
+    filterCards() {
+      return this.cards.filter((card) => {
+        return card.tag.includes(this.filterItem);
+      });
+    },
+  },
+  methods: {
+    clickTag(tag) {
+      this.filterItem = tag;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Jost", sans-serif;
+  font-family: "DM Serif Display", serif;
+
+  .center {
+    padding-left: calc(50% - (1200px / 2));
+    padding-right: calc(50% - (1200px / 2));
+  }
+
+  a {
+    text-decoration: none;
+  }
+  .button__checked {
+    border: none;
+    padding: 26px 66px;
+    color: #292f36;
+    text-align: center;
+    font-family: Jost;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 125%; 
+    letter-spacing: 0.36px;
+    border-radius: 18px;
+    background: #ffffff;
+    &:hover {
+      color: #fff;
+      background: #cda274;
+    }
+  }
+}
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,57 @@
+<template>
+  <header class="top-header center">
+    <div class="top-header__wrapper">
+      <div class="top-header__logo">
+        <a href="index.html"><img src="../img/Logo.svg" alt="Logo" /></a>
+      </div>
+      <div class="top-header__menu">
+        <a href="index.html" class="top-header__link">Home </a>
+        <a href="#" class="top-header__link">Project</a>
+        <a href="blog.html" class="top-header__link">Blog</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="css" scoped>
+.top-header {
+  padding-top: 53px;
+  padding-bottom: 56px;
+}
+.top-header__wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+
+.top-header__menu {
+  display: flex;
+  gap: 25px;
+}
+
+.top-header__link {
+  color: #292f36;
+  text-align: center;
+  font-family: Jost;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 125%;
+}
+.top-header__link:hover {
+  color: #cda274;
+}
+</style>
